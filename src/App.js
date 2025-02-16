b444e94cfaf0663c5fc47cf845fe0c824a444d27
Heramb Chaudhari <169079872+Heramb1221@users.noreply.github.com>
2025-02-16 11:41:22 +0530

    Navbar Structure

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+import { useState } from "react";
+import {Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return <div>
+    <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<Signup />} />
+      <Route path="dashboard" element={<Dashboard />} />
+    </Routes>
+  </div>;
+}
+
+export default App;
